cd31b5df8812766bf410047c3d9f1da2b32dcf59
alvin lal <alvinzzz2001@gmail.com>
2020-03-07 13:14:45 +0530

    test2

---
+++ build/precache-manifest.7701c691e098db8ea7c78a321cec6005.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f8c052819a7a91fc0edd12f17182fbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbcd39d5334619414674",
+    "url": "/static/css/main.fb5e79b7.chunk.css"
+  },
+  {
+    "revision": "34e71653bfad86c6ccd8",
+    "url": "/static/js/2.18480ae7.chunk.js"
+  },
+  {
+    "revision": "5c2176e7db1837929debf21c65634f23",
+    "url": "/static/js/2.18480ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbcd39d5334619414674",
+    "url": "/static/js/main.4e934b07.chunk.js"
+  },
+  {
+    "revision": "99e47101cc2d1764cd87",
+    "url": "/static/js/runtime-main.9295b42a.js"
+  },
+  {
+    "revision": "b7445e2a6ad8cb1633da12d50cb6f13a",
+    "url": "/static/media/paper.b7445e2a.png"
+  },
+  {
+    "revision": "a94e17c01ef127ae05d31fc9fd083843",
+    "url": "/static/media/rock.a94e17c0.png"
+  },
+  {
+    "revision": "0af33929a7a2bd1207746722f62ea35c",
+    "url": "/static/media/scissors.0af33929.png"
+  }
+]);
